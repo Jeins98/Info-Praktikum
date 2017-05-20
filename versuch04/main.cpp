87448d42be626715fc811a223150386d8be532e8
Jeins98 <jonasschulz989@gmail.com>
2017-05-20 10:28:25 +0200

    Update main.cpp

--- versuch04/main.cpp
+++ versuch04/main.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////
 // Praktikum Informatik 1 MMXVII
-// Versuch 04: Einführung Klasse
+// Versuch 04: EinfÃ¼hrung Klasse
 //
 // Datei:  main.cpp
 // Inhalt: Hauptprogramm
@@ -30,14 +30,14 @@ int main()
 
 	Vektor earth(0, 6371, 0);
 	Vektor person(0, 0.8318, 0);
-	Vektor eye = earth.add(person);
-	Vektor sight = earth.sub(eye);
+	Vektor person1 = earth.add(person);
+	Vektor sight = earth.sub(person1);
 	int zeahler = 0;
 
 	while(sight.scalarProd(earth) >= 0.01 || sight.scalarProd(earth) <= -0.01)
 	{
 		earth.rotateAroundZ(0.00000001);
-		sight = earth.sub(eye);
+		sight = earth.sub(person1);
 		rad += 0.00000001;
 		zeahler ++;
 	}
@@ -46,9 +46,9 @@ int main()
 
 	std::cout << std::fixed;
 
-	std::cout << "Sie können " << sightlength << " Km weit sehen." << std::endl;
+	std::cout << "Sie kÃ¶nnen " << sightlength << " Km weit sehen." << std::endl;
 	std::cout << "Sie sind " << std::setprecision(3) << person.getY() * 1000 << " Meter hoch." << std::endl;
-	std::cout << "Der Winkel beträgt " << rad * 180 / PI << " Grad." << std::endl;
+	std::cout << "Der Winkel betrÃ¤gt " << rad * 180 / PI << " Grad." << std::endl;
 	std::cout << "Anzhal Steps: " << zeahler << std::endl;
 
    return 0;
